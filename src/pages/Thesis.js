424807aad2f09ecdc2fd6dc5b692ea68336bae3b
ImGonnaPuke <ahorle@oakland.edu>
2023-04-14 01:00:55 -0400

    thesis

--- src/pages/Thesis.js
+++ src/pages/Thesis.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './pages.css'
+import JSZip from 'jszip';
 
 function Thesis() {
-  return (
-    <div className='thesis'>Thesis</div>
+
+//Geer-Thesis Jpegs.zip
+
+return (
+    <div className='thesis'>
+      <h1><a href='/Geer-Thesis Jpegs.zip' download>  Download Thesis</a></h1>
+    </div>
   )
 }
 
