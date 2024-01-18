a723b71a3ce875e8a61354cd2526f4959088d3cc
ImGonnaPuke <ahorle@oakland.edu>
2024-01-18 02:39:38 -0500

    Contact page push

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,8 +6,9 @@ function Contact() {
     <div className='contact'>
         <h2>Contact Info:</h2>
        <h1>Kate Geer</h1> 
-       <h3>email: kgeerartservices@gmail.com</h3>
-       <h3>instagram: <a href='https://www.instagram.com/geerkate/?hl=en'>GeerKate</a></h3>
+       <h3>Email: kgeerartservices@gmail.com</h3>
+       <h3>Instagram: <a href='https://www.instagram.com/geerkate/?hl=en'>GeerKate</a></h3>
+       <h3>LinkedIn: <a href='https://www.linkedin.com/in/kaitlyn-geer-3168511a1/'>Kaitlyn Geer</a></h3>
         </div>
   )
 }
